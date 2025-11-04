af6437fbadd910387bcd74f38e1e519bff0e304a
Joao <58124203+J-NSC@users.noreply.github.com>
2025-11-04 09:51:01 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 
-Route::middleware(['auth'])->group(function () {
-    Route::prefix('agenda')->name('contact.')->group(function () {
-        Route::get('/',  [ContactController::class, 'index'])->name('index');
-        Route::post('/store',  [ContactController::class, 'store'])->name('store');
-        Route::put('/{contact}',  [ContactController::class, 'update'])->name('update');
-        Route::delete('/{contact}', [ContactController::class, 'destroy'])->name('destroy');
-    });
-}
-
 Route::middleware('auth')->group(function () {
+
+     Route::prefix('agenda')->name('contact.')->group(function () {
+            Route::get('/',  [ContactController::class, 'index'])->name('index');
+            Route::post('/store',  [ContactController::class, 'store'])->name('store');
+            Route::put('/{contact}',  [ContactController::class, 'update'])->name('update');
+            Route::delete('/{contact}', [ContactController::class, 'destroy'])->name('destroy');
+    });
+
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
